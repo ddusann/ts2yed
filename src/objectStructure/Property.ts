6f7abbb29f31a980247de91f47965e53e1e39f6f
Dušan Kováčik <ddusann7@gmail.com>
2019-03-16 10:06:27 +0100

    Add objectStructure files

---
+++ src/objectStructure/Property.ts
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2019 Dušan Kováčik
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import VisibilityType from './VisibilityType';
+
+export default class Property {
+    private _isAbstract: boolean;
+    private _isStatic: boolean;
+    private _name: string;
+    private _type: string;
+    private _visibility: VisibilityType;
+
+    constructor() {
+        this._name = name;
+        this._visibility = VisibilityType.PUBLIC;
+        this._isAbstract = false;
+        this._isStatic = false;
+        this._type = 'any';
+    }
+
+    getName(): string {
+        return this._name;
+    }
+
+    getType(): string {
+        return this._type;
+    }
+
+    getVisibility(): VisibilityType {
+        return this._visibility;
+    }
+
+    isAbstract(): boolean {
+        return this._isAbstract;
+    }
+
+    isStatic(): boolean {
+        return this._isStatic;
+    }
+}
