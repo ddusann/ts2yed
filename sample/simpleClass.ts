d1777e62e97bfa44aa99457c66e24c7619e8cc06
Dušan Kováčik <ddusann7@gmail.com>
2019-03-16 10:09:03 +0100

    Update samples

---
+++ sample/simpleClass.ts
@@ -0,0 +1,11 @@
+export default class ClassName {
+    private _attributeName: string;
+
+    constructor() {
+        this._attributeName = 'abc';
+    }
+
+    methodName(): number {
+        return 14;
+    }
+}
