4a1c9f887b9de9390eebb4822efc6a9f88cac59f
Dušan Kováčik <ddusann7@gmail.com>
2019-03-24 23:13:47 +0100

    Update samples

--- sample/projects/abstractClass/Base.ts
+++ sample/projects/abstractClass/Base.ts
@@ -1,7 +1,12 @@
-export abstract class Base {
+export abstract class Base<T> {
+    static counter: number = 0;
     protected _smth: number;
+    private _optional?: boolean;
 
     constructor() {
+        ++Base.counter;
         this._smth = 42;
     }
+
+    abstract getNumber(): number;
 }
--- sample/projects/abstractClass/Child.ts
+++ sample/projects/abstractClass/Child.ts
@@ -1,7 +1,15 @@
 import { Base } from './Base';
 
-export class Child extends Base {
+export class Child extends Base<boolean> {
+    private readonly _type: string;
+
     constructor() {
         super();
+
+        this._type = 'abc';
+    }
+
+    getNumber(): number {
+        return 43;
     }
 }
