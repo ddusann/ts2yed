d04aa8b52a5b3d351f248eb22078ec956059b2eb
Dušan Kováčik <ddusann7@gmail.com>
2019-03-24 12:06:56 +0100

    Update samples

--- sample/projects/twoClasses/First.ts
+++ sample/projects/twoClasses/First.ts
@@ -1,6 +1,14 @@
 export default class First {
     private _name: string;
 
+    get name(): string {
+        return this._name;
+    }
+
+    set name(name: string) {
+        this._name = name;
+    }
+
     constructor(name: string) {
         this._name = name;
     }
